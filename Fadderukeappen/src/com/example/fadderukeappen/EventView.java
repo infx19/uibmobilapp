0eb4340ebfcb57910ef6add0c8cb7de76858597d
mgr090 <mariannegrov@gmail.com>
2013-03-08 12:52:02 +0100

    la til EventView

---
+++ Fadderukeappen/src/com/example/fadderukeappen/EventView.java
@@ -0,0 +1,13 @@
+package com.example.fadderukeappen;
+
+import android.content.Context;
+import android.view.View;
+
+public class EventView extends View {
+
+	public EventView(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+}
