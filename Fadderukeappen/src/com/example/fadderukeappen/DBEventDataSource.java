0762d0f3a047edf60abc5c0b680aed4885543b43
JohRus <johan.rusvik@gmail.com>
2013-03-11 15:17:53 +0100

    created DBEventDataSource class

---
+++ Fadderukeappen/src/com/example/fadderukeappen/DBEventDataSource.java
@@ -0,0 +1,10 @@
+package com.example.fadderukeappen;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
+
+public class DBEventDataSource {
+	
+	private SQLiteDatabase database;
+	private DBEventHelper dbEventHelper;
+}
