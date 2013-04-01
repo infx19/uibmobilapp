ea650ed92d2524391901d5b5290d21e46e599277
JohRus <johan.rusvik@gmail.com>
2013-04-01 20:47:55 +0200

    created EventTest, modified .classpath and added mockito jar to libs

---
+++ Fadderukeappen/test/com/example/fadderukeappen/EventTest.java
@@ -0,0 +1,19 @@
+package com.example.fadderukeappen;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class EventTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+	}
+
+}
