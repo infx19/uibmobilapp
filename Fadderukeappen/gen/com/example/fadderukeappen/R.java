c3f04bdfd269cc341a3dd6287087c61b35d73eab
mgr090 <mariannegrov@gmail.com>
2013-04-07 10:19:01 +0200

    Fikset fling og lagt til header i dagsvisning

--- Fadderukeappen/gen/com/example/fadderukeappen/R.java
+++ Fadderukeappen/gen/com/example/fadderukeappen/R.java
@@ -25,11 +25,12 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int activity_day_linear_layout=0x7f070000;
-        public static final int activity_daylist_linear_layout=0x7f070001;
-        public static final int dummy_button=0x7f070004;
-        public static final int fullscreen_content=0x7f070002;
-        public static final int fullscreen_content_controls=0x7f070003;
+        public static final int TextViewHeader=0x7f070000;
+        public static final int activity_day_linear_layout=0x7f070001;
+        public static final int activity_daylist_linear_layout=0x7f070002;
+        public static final int dummy_button=0x7f070005;
+        public static final int fullscreen_content=0x7f070003;
+        public static final int fullscreen_content_controls=0x7f070004;
     }
     public static final class layout {
         public static final int activity_day=0x7f030000;
--- Fadderukeappen/src/com/example/fadderukeappen/DayActivity.java
+++ Fadderukeappen/src/com/example/fadderukeappen/DayActivity.java
@@ -128,29 +128,52 @@ public class DayActivity extends Activity {
 		private static final int SWIPE_MAX_OFF_PATH = 250;
 		private static final int SWIPE_THRESHOLD_VELOCITY = 100;
 		@Override
-		    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
-		            float velocityY) {
-		        Log.d("---onFling---", e1.toString() + e2.toString() + "");
+		public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+				float velocityY) {
+			//Log.d("---onFling---", e1.toString() + e2.toString() + "");
 
-		        try {
-		            if (Math.abs(e1.getY() - e2.getY()) > SWIPE_MAX_OFF_PATH)
-		                return false;
-		            // right to left swipe
-		            if (e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE
-		                    && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
-		                //do your code
+			try {
+				if (Math.abs(e1.getY() - e2.getY()) > SWIPE_MAX_OFF_PATH)
+					return false;
+				// right to left swipe
+				if (e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE
+						&& Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+					if (date.equals(Controller.FADDERUKE_END_DATE)) {
+						return false;
+					}
+					Date d = date.nextDate();
+					Log.v("fling", "FLING to " + d.toString() + velocityX);
 
-		            } else if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE
-		                    && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
-		                //left to right flip
-		                }
-		            } catch (Exception e) {
-		            // nothing
-		        }
-		        return false;
-		    }
-		
-		
+					Intent intent = new Intent(DayActivity.this, DayActivity.class);
+					intent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+					intent.putExtra("com.example.fadderukeappen.daylist", d.toString());
+					startActivity(intent);
+					dbEventDataSource.close();
+					return true;
+
+				} else if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE
+						&& Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+					if (date.equals(Controller.FADDERUKE_START_DATE)) {
+						Log.v("DATE", "start date");
+						return false;
+					}
+					Date d = date.prevDate();
+					Log.v("fling", "FLING to " + d.toString() + velocityX);
+
+					Intent intent = new Intent(DayActivity.this, DayActivity.class);
+					intent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+					intent.putExtra("com.example.fadderukeappen.daylist", d.toString());
+					startActivity(intent);
+					dbEventDataSource.close();
+					return true;
+				}
+			} catch (Exception e) {
+				// nothing
+			}
+			return false;
+		}
+
+
 		/*@Override
 		public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
 			if (velocityX >= 1000) {
