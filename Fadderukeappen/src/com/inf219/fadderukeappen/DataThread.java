8f8f39fbaa313d9b1ea1f35b27c46320229bc804
JohRus <johan.rusvik@gmail.com>
2013-05-22 12:24:10 +0200

    changed name from DownloadInfoTask to DataThread. Did some refactoring in Controller

---
+++ Fadderukeappen/src/com/inf219/fadderukeappen/DataThread.java
@@ -0,0 +1,25 @@
+package com.inf219.fadderukeappen;
+
+import java.net.URL;
+import java.util.ArrayList;
+
+import android.os.AsyncTask;
+
+class DataThread extends AsyncTask<String, Integer, ArrayList<Event>> {
+	protected ArrayList<Event> doInBackground(String... urls) {
+		int count = urls.length;
+		ArrayList<Event> events = null;
+		for (int i = 0; i < count; i++) {
+			events = XMLParser.getEventsInfoFromURL(urls[i]);
+		}
+		return events;
+	}
+
+	protected void onProgressUpdate(Integer... progress) {
+		// setProgressPercent(progress[0]);
+	}
+
+	protected void onPostExecute(Long result) {
+		// showDialog("Downloaded " + result + " bytes");
+	}
+}
