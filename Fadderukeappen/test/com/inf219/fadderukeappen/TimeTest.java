b6e4740af8a3172c92c07737080a52f4bdb71c9f
mgr090 <mariannegrov@gmail.com>
2013-06-11 10:34:58 +0200

    copyright

--- Fadderukeappen/test/com/inf219/fadderukeappen/TimeTest.java
+++ Fadderukeappen/test/com/inf219/fadderukeappen/TimeTest.java
@@ -8,6 +8,25 @@ import org.junit.Test;
 import com.inf219.fadderukeappen.HoursAndMins;
 import com.inf219.fadderukeappen.Time;
 
+/**
+ * Copyright 2013 Marianne Grov and Johan Rusvik
+ * 
+ * This file is part of Fadderukeappen.
+ * Fadderukeappen is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Fadderukeappen is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Fadderukeappen. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 public class TimeTest {
 
 	private Time testObject1;
